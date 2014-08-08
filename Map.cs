4afdeb51eed3392fd9e2952e8aab34d4622f5265
valiman <valiman@live.se>
2014-08-08 16:13:19 +0200

    Proj
    proj

---
+++ Map.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace LangtonsAnt
+{
+    public class Map
+    {
+        public List<Tile> tileList = new List<Tile>();
+        private Graphics graphics;
+        public Map (Graphics graphics)
+        {
+            this.graphics = graphics;
+        }
+        public void DrawTile(Tile tile, Point point, TileColor tileColor)
+        {
+            Brush tileBrush;
+            switch (tileColor)
+            {
+                case TileColor.White:
+                    tileBrush = Brushes.Wheat;
+                    break;
+                case TileColor.Black:
+                    tileBrush = Brushes.Black;
+                    break;
+                case TileColor.Red:
+                    tileBrush = Brushes.Red;
+                    break;
+                case TileColor.Green:
+                    tileBrush = Brushes.Green;
+                    break;
+                case TileColor.Blue:
+                    tileBrush = Brushes.Blue;
+                    break;
+                default:
+                    tileBrush = Brushes.White;
+                    break;
+            }
+
+            //Size
+            Size size = new Size(TileSize.X, TileSize.Y);
+
+            //Drawing object
+            tile.Rectangle = new Rectangle(point, size);
+            this.graphics.FillRectangle(tileBrush, tile.Rectangle);
+        }
+    }
+}
---
+++ Tile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace LangtonsAnt
+{
+    public class Tile
+    {
+        private TileColor colors;
+        private Point point;
+        private Rectangle rectangle;
+
+        public Tile(Point p, TileColor colors)
+        {
+            this.point = p;
+            this.colors = colors;
+        }
+        public TileColor Colors { 
+            get { return this.colors; } 
+            set { this.colors = value; } 
+        }
+        public Point Point { 
+            get { return this.point; } 
+            set { this.point = value; } 
+        }
+        public Rectangle Rectangle { 
+            get { return this.rectangle; } 
+            set { this.rectangle = value; } 
+        }
+    }
+}
